a3a0c80a5b42b76ab3a0efa58e24f6e0849877f7
Terrance <git@terrance.allofti.me>
2018-07-04 21:21:07 +0100

    Add CLI option to write config on exit

--- immp/__main__.py
+++ immp/__main__.py
@@ -1,3 +1,4 @@
+from argparse import ArgumentParser
 import asyncio
 import logging
 import logging.config
@@ -23,17 +24,7 @@ class LocalFilter(logging.Filter):
         return record.name == "__main__" or record.name.startswith("immp.")
 
 
-def main(config):
-    for path in config["path"]:
-        sys.path.append(path)
-    if config["logging"]:
-        logging.config.dictConfig(config["logging"])
-    else:
-        logging.basicConfig(level=logging.INFO)
-        for handler in logging.root.handlers:
-            handler.addFilter(LocalFilter())
-    log = logging.getLogger(__name__)
-    log.info("Creating plugs and hooks")
+def config_to_host(config):
     host = Host()
     for name, spec in config["plugs"].items():
         cls = resolve_import(spec["path"])
@@ -44,6 +35,46 @@ def main(config):
     for name, spec in config["hooks"].items():
         cls = resolve_import(spec["path"])
         host.add_hook(cls(name, spec["config"], host))
+    return host
+
+
+def host_to_config(host):
+    config = {"plugs": {}, "channels": {}, "hooks": {}}
+    for name, plug in host.plugs.items():
+        if plug.virtual:
+            continue
+        path = "{}.{}".format(plug.__class__.__module__, plug.__class__.__name__)
+        config["plugs"][name] = {"path": path, "config": plug.config}
+    for name, channel in host.channels.items():
+        if channel.plug.virtual:
+            continue
+        config["channels"][name] = {"plug": channel.plug.name, "source": channel.source}
+    for cls, hook in host.resources.items():
+        path = "{}.{}".format(cls.__module__, cls.__name__)
+        config["hooks"][hook.name] = {"path": path, "config": hook.config}
+    for name, hook in host.hooks.items():
+        path = "{}.{}".format(hook.__class__.__module__, hook.__class__.__name__)
+        config["hooks"][name] = {"path": path, "config": hook.config}
+    return config
+
+
+def init(logs, paths):
+    if logs:
+        logging.config.dictConfig(logs)
+    else:
+        logging.basicConfig(level=logging.INFO)
+        for handler in logging.root.handlers:
+            handler.addFilter(LocalFilter())
+    for search in paths:
+        sys.path.append(search)
+
+
+def main(path, write=False):
+    config = _schema(anyconfig.load(path))
+    init(config["logging"], config["path"])
+    log = logging.getLogger(__name__)
+    host = config_to_host(config)
+    log.info("Creating plugs and hooks")
     loop = asyncio.get_event_loop()
     task = loop.create_task(host.run())
     try:
@@ -55,9 +86,19 @@ def main(config):
         loop.run_until_complete(task)
     finally:
         loop.close()
+        if write:
+            log.info("Writing config file")
+            config.update(host_to_config(host))
+            anyconfig.dump(config, path)
+
+
+def parse():
+    parser = ArgumentParser(prog="python -m immp", add_help=False)
+    parser.add_argument("-w", "--write", action="store_true")
+    parser.add_argument("file", metavar="FILE")
+    return parser.parse_args()
 
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        exit("Usage: python -m immp <config file>")
-    main(_schema(anyconfig.load(sys.argv[1])))
+    args = parse()
+    main(args.file, args.write)
