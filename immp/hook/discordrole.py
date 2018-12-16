3f9254e694b95e4ccd8df65cf51d144ebf444f6c
Terrance <git@terrance.allofti.me>
2018-12-16 21:04:47 +0000

    Minimal couple of new plug-specific hooks

---
+++ immp/hook/discordrole.py
@@ -0,0 +1,76 @@
+"""
+Self-serve Discord roles for users.
+
+Config:
+    roles (dict):
+        Mapping from user-facing role names to Discord role IDs.
+
+Commands:
+    role <name>:
+        Claim a role of this name.
+    unrole <name>:
+        Drop the role of this name.
+"""
+
+from voluptuous import ALLOW_EXTRA, Schema
+
+import immp
+from immp.hook.command import CommandParser, CommandScope, command
+from immp.plug.discord import DiscordPlug
+
+
+class _Schema:
+
+    config = Schema({"roles": {str: int}}, extra=ALLOW_EXTRA, required=True)
+
+
+class _NoSuchRole(Exception):
+    pass
+
+
+class DiscordRoleHook(immp.Hook):
+    """
+    Hook to assign and unassign Discord roles to and from users.
+    """
+
+    def __init__(self, name, config, host):
+        super().__init__(name, _Schema.config(config), host)
+
+    def _common(self, msg, name):
+        if name not in self.config["roles"]:
+            raise _NoSuchRole
+        client = msg.channel.plug._client
+        channel = client.get_channel(int(msg.channel.source))
+        member = channel.guild.get_member(int(msg.user.id))
+        for role in channel.guild.roles:
+            if role.id == self.config["roles"][name]:
+                return role, member
+        else:
+            raise _NoSuchRole
+
+    def _test(self, channel, user):
+        return isinstance(channel.plug, DiscordPlug)
+
+    @command("role", scope=CommandScope.shared, parser=CommandParser.none,
+             test=_test, sync_aware=True)
+    async def role(self, msg, name):
+        try:
+            role, member = self._common(msg, name)
+        except _NoSuchRole:
+            await msg.channel.send(immp.Message(text="No such role"))
+            return
+        else:
+            await member.add_roles(role)
+            await msg.channel.send(immp.Message(text="\N{WHITE HEAVY CHECK MARK} Added"))
+
+    @command("unrole", scope=CommandScope.shared, parser=CommandParser.none,
+             test=_test, sync_aware=True)
+    async def unrole(self, msg, name):
+        try:
+            role, member = self._common(msg, name)
+        except _NoSuchRole:
+            await msg.channel.send(immp.Message(text="No such role"))
+            return
+        else:
+            await member.remove_roles(role)
+            await msg.channel.send(immp.Message(text="\N{WHITE HEAVY CHECK MARK} Removed"))
---
+++ immp/hook/hangoutslock.py
@@ -0,0 +1,76 @@
+"""
+History and link-join setting locks for Hangouts.
+
+Config:
+    history (dict):
+        Mapping from channel names to desired conversation history settings -- ``True`` to keep
+        history enabled, ``False`` to keep it disabled.
+    linkjoin (dict):
+        Mapping from channel names to desired link join settings -- ``True`` to enable joining the
+        hangout via link, ``False`` to disable it.
+"""
+
+import hangups
+from hangups import hangouts_pb2
+from voluptuous import ALLOW_EXTRA, Optional, Schema
+
+import immp
+from immp.plug.hangouts import HangoutsPlug
+
+
+HISTORY = {True: hangouts_pb2.OFF_THE_RECORD_STATUS_ON_THE_RECORD,
+           False: hangouts_pb2.OFF_THE_RECORD_STATUS_OFF_THE_RECORD}
+
+LINK_JOIN = {True: hangouts_pb2.GROUP_LINK_SHARING_STATUS_ON,
+             False: hangouts_pb2.GROUP_LINK_SHARING_STATUS_OFF}
+
+
+class _Schema:
+
+    config = Schema({Optional("history", default=dict): {str: bool},
+                     Optional("linkjoin", default=dict): {str: bool}},
+                    extra=ALLOW_EXTRA, required=True)
+
+
+class HangoutsLockHook(immp.Hook):
+    """
+    Hook to enforce the history and link-join settings in Hangouts.
+    """
+
+    def __init__(self, name, config, host):
+        super().__init__(name, _Schema.config(config), host)
+
+    @property
+    def channels(self):
+        try:
+            return {key: {self.host.channels[label]: setting
+                          for label, setting in mapping.items()}
+                    for key, mapping in self.config.items()}
+        except KeyError as e:
+            raise immp.HookError("No channel named '{}'".format(e.args[0]))
+
+    async def on_receive(self, sent, source, primary):
+        await super().on_receive(sent, source, primary)
+        if sent != source or not isinstance(sent.channel.plug, HangoutsPlug):
+            return
+        conv = sent.channel.plug._convs.get(sent.channel.source)
+        if isinstance(sent.raw, hangups.OTREvent):
+            setting = HISTORY.get(self.channels["history"].get(sent.channel))
+            if setting is None:
+                return
+            if setting != sent.raw.new_otr_status:
+                request = hangouts_pb2.ModifyOTRStatusRequest(
+                    request_header=sent.channel.plug._client.get_request_header(),
+                    event_request_header=conv._get_event_request_header(),
+                    otr_status=setting)
+                await sent.channel.plug._client.modify_otr_status(request)
+        elif isinstance(sent.raw, hangups.GroupLinkSharingModificationEvent):
+            setting = LINK_JOIN.get(self.channels["linkjoin"].get(sent.channel))
+            if setting is None:
+                return
+            if setting != sent.raw.new_status:
+                request = hangouts_pb2.SetGroupLinkSharingEnabledRequest(
+                    request_header=sent.channel.plug._client.get_request_header(),
+                    event_request_header=conv._get_event_request_header(),
+                    group_link_sharing_status=setting)
+                await sent.channel.plug._client.set_group_link_sharing_enabled(request)
